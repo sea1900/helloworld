ce914b49ba4bdb9947107a7b28effc6a8e42a5c5
hai chen <hailang-250@163.com>
2013-10-18 13:11:19 +0800

    C先生改的

--- src/com/hsea/test/Test.java
+++ src/com/hsea/test/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is my first git repositoryB");
+		System.out.println("This is my first git repositoryC");
 	}
 
 }
