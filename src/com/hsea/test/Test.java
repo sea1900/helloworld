273a2fc4d2df8f57c70c1c0774a6cdcc5662ebdf
hai chen <hailang-250@163.com>
2013-10-18 11:19:51 +0800

    test2

---
+++ src/com/hsea/test/Test.java
@@ -0,0 +1,13 @@
+package com.hsea.test;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first git repository");
+	}
+
+}
